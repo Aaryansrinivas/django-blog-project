9fd41817d4a02368a6daf1093805f626585a15b8
Aaryansrinivas <aaryansrinivas16@gmail.com>
2025-08-07 10:11:55 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,11 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+82+wv_s^va=a-%(k$r4zv&43w*&k+=f)68_p(0_!gpabhtn(^'
 
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.environ.get('DEBUG', 'True') == 'True'
+DEBUG =os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
+ALLOWED_HOSTS = 'django-blog-project.onrender.com',
+    '127.0.0.1',
+    'localhost',
+
 
 
 
@@ -155,5 +160,6 @@ LOGIN_URL = 'login'
 #CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 #CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 
